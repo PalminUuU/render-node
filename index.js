72813a94f14f8b134ba6e91d30439693f2f2c92e
José Antonio Malpica Berthely <jose.malpica@conavimex.local>
2023-08-21 11:07:46 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("el servidor esta conectado en el puerto " + port);
+});
+
+app.get("/", (req,res) =>{
+    res.send("Hola, render")
+})
