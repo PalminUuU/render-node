7e15adcc7b2a0189889f48a360e0f2297ea323ea
José Antonio Malpica Berthely <jose.malpica@conavimex.local>
2023-08-21 11:54:13 -0600

    integracion pg

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 import express from "express";
 import cors from "cors";
+import { config } from "dotenv";
+import pg from "pg";
 const app = express();
 
 app.use(cors());
+
+config()
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("el servidor esta conectado en el puerto " + port);
 });
 
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+})
+
 app.get("/", (req,res) =>{
     res.send("Hola, render y ramon es gay")
+})
+
+app.get("/ping", async (req,res) =>{
+ const result = await pool.query('SELECT NOW ()')
+ return res.json(result.rows[0])
 })
