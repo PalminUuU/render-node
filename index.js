2821821d0db3eda0e3612647d1ce09ac8add285a
José Antonio Malpica Berthely <jose.malpica@conavimex.local>
2023-10-09 11:57:34 -0600

    apis new

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ import cors from "cors";
 import { config } from "dotenv";
 import pg from "pg";
 import jwt from "jsonwebtoken"
+import bodyParser from "body-parser";
+import argon2 from "argon2";
 
 const app = express();
 app.use(cors());
 config()
 const secret =process.env.SECRET
 const port = process.env.PORT || 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.listen(port, () => {
   console.log("el servidor esta conectado en el puerto " + port);
 });
@@ -30,21 +36,27 @@ app.get("/ping", async (req,res) =>{
 app.get("/consulta/:email/:password", async (req, res) => {
   try {
     const email = req.params.email;
-    const password = req.params.password;
-
-    const sqlSelect = "SELECT * FROM public.users WHERE correo = $1 AND contraseña = $2";
-    const result = await pool.query(sqlSelect, [email, password]);
+    const providedPassword = req.params.password;
+    const sqlSelect = "SELECT * FROM public.users WHERE correo = $1";
+    const result = await pool.query(sqlSelect, [email]);
 
     if (result.rows.length === 0) {
-      res.send('[{"nombre": "INEXISTENTE"}]');
+      res.send({token: "", message: "Usuario Incorrecto" });
     } else {
       const user = result.rows[0];
-      const token = jwt.sign({
-        nombre: user.nombre,
-        correo: user.correo,
-        exp: Date.now() + 24 * 60 * 60 * 1000, 
-      }, secret);
-      res.send(token);
+      const isPasswordValid = await argon2.verify(user.contraseña, providedPassword);
+
+      if (isPasswordValid) {
+        const token = jwt.sign({
+          nombre: user.nombre,
+          correo: user.correo,
+          exp: Date.now() + 24 * 60 * 60 * 1000, 
+        }, secret);
+
+        res.send({token:token});
+      } else {
+        res.send({token:"" , message: "Contraseña incorrecta" });
+      }
     }
   } catch (error) {
     console.error("Error en la consulta:", error);
@@ -57,10 +69,6 @@ app.get("/private/:token", (req,res) =>{
  try {
   const token = req.params.token
   const payload = jwt.verify(token, secret)
-
-  const nombre = payload.nombre;
-  const correo = payload.correo;
-
   if (Date.now() > payload.exp){
     return res.status(401).send({error : "token expired"})
   }
@@ -70,3 +78,28 @@ app.get("/private/:token", (req,res) =>{
   res.status(401).send({error: error.message})
  }
 })
+
+
+
+app.post("/register/", async (req, res) => {
+  try {
+    const nombre = req.body.nombre;
+    const correo = req.body.correo;
+    const contraseña = req.body.contraseña;
+    const hash = await argon2.hash(contraseña);
+
+    const insertQuery =
+    `INSERT INTO public.users (nombre, correo, contraseña)
+      VALUES ($1, $2, $3)
+      RETURNING id, nombre, correo;`;
+
+    const result = await pool.query(insertQuery, [nombre, correo, hash]);
+
+    const insertedUser = result.rows[0];
+
+    res.status(201).json(insertedUser);
+  } catch (error) {
+    console.error("Error en el registro:", error);
+    res.status(500).json({ message: "Error en el registro" });
+  }
+});
